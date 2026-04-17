c24bb19f105f71a054651f40d6bee06f075bd18a
Sachin <sachrohile@gmail.com>
2026-04-17 16:43:02 +0530

    Add constants and improve main function documentation in main.rs; update .gitignore to exclude target directory

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,24 @@
+const THRE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
     println!("Hello, world!");
+    // Constant
+    /*
+       1. Constant are always immutable
+       2. We must write the type explicitly.
+       3. Constant can be declared at any scope. including global scope.
+       4. The value must be compile time constant.
+    */
+
+    const MAX_POINT: u32 = 100_000;
+    println!("{MAX_POINT}");
+
+    println!("Three hourse in seconds: {THRE_HOURS_IN_SECONDS}");
 
     // Shadowing
+    /*
+       1.
+    */
     let t = 10;
     let t = t + 10;
     println!("The value of t is: {}", t);
